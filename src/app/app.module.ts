e61fc3be49657f804a14dd19199751b264772543
Payson Parker <paysonparker@costco.com>
2023-10-10 08:50:13 -0700

    primeNg and bootstrap setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { NavigationBarComponent } from './core/components/navigation-bar/navigat
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
   ],
