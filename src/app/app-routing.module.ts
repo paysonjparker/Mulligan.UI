e1a0be11161c900d8153d9ad264d36634e2536aa
Payson Parker <paysonparker@costco.com>
2023-10-10 11:10:59 -0700

    not ofund routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/components/home/home.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
 const routes: Routes = [
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
   {
     path: '',
     component: HomeComponent
