4ea61b0d3f51f80c6b43c73657fa09c33895f214
Payson Parker <paysonparker@costco.com>
2024-03-11 09:45:30 -0700

    removed bad code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './core/components/home/home.component';
-import { NotFoundComponent } from './core/components/not-found/not-found.component';
-import { UserProfilePageComponent } from './core/components/user-components/user-profile-page/user-profile-page.component';
-import { UserListComponent } from './core/components/user-components/user-list/user-list.component';
-import { CreateUserComponent } from './core/components/user-components/create-user/create-user.component';
-import { GolfCourseListComponent } from './core/components/golf-course-components/golf-course-list/golf-course-list.component';
-import { GolfCourseProfilePageComponent } from './core/components/golf-course-components/golf-course-profile-page/golf-course-profile-page.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'users',
-    component: UserListComponent
-  },
-  {
-    path: 'users/:Id',
-    component: UserProfilePageComponent
-  },
-  {
-    path: 'createUser',
-    component: CreateUserComponent
-  },
-  {
-    path: 'golfCourses',
-    component: GolfCourseListComponent
-  },
-  {
-    path: 'golfCourses/:Id',
-    component: GolfCourseProfilePageComponent
-  },
-  {
-    path: '**',
-    component: NotFoundComponent
-  },
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,38 +4,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './core/components/home/home.component';
-import { NotFoundComponent } from './core/components/not-found/not-found.component';
-import { NavigationBarComponent } from './core/components/navigation-bar/navigation-bar.component';
-import { UserProfilePageComponent } from './core/components/user-components/user-profile-page/user-profile-page.component';
-import { UserListComponent } from './core/components/user-components/user-list/user-list.component';
-import { CreateUserComponent } from './core/components/user-components/create-user/create-user.component';
-import { GolfCourseListComponent } from './core/components/golf-course-components/golf-course-list/golf-course-list.component';
-import { GolfCourseProfilePageComponent } from './core/components/golf-course-components/golf-course-profile-page/golf-course-profile-page.component';
-
-// PrimeNg Modules
-import { MenubarModule } from 'primeng/menubar';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { InputTextModule } from 'primeng/inputtext';
-import { DropdownModule } from 'primeng/dropdown';
-import { CardModule } from 'primeng/card';
-import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NotFoundComponent,
-    NavigationBarComponent,
-    UserProfilePageComponent,
-    UserListComponent,
-    CreateUserComponent,
-    GolfCourseListComponent,
-    GolfCourseProfilePageComponent,
   ],
   imports: [
     BrowserModule,
@@ -44,13 +18,6 @@ import { ToastModule } from 'primeng/toast';
     ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
-    MenubarModule,
-    ButtonModule,
-    TableModule,
-    InputTextModule,
-    DropdownModule,
-    CardModule,
-    ToastModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
